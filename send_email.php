cbadaefe9249b4348937ae3bcca0370265c31437
Elias <150734575+Sir-Eddy@users.noreply.github.com>
2024-12-03 16:49:53 +0100

    Updated form

--- send_email.php
+++ send_email.php
@@ -1,4 +1,6 @@
 <?php
+$messageStatus = ""; // Variable für Statusmeldungen
+
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     // Formularfelder abrufen und validieren
     $fullname = htmlspecialchars($_POST['fullname']); // Name des Absenders
@@ -7,33 +9,30 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 
     // Überprüfen, ob die Felder ausgefüllt sind
     if (empty($fullname) || empty($email) || empty($message)) {
-        echo "Bitte füllen Sie alle Felder aus.";
-        exit;
-    }
-
-    // Empfängeradresse und Betreff
-    $to = "mail@about-elias.de"; // Zieladresse
-    $subject = "Neue Nachricht von $fullname"; // Betreff
-
-    // Nachricht formatieren
-    $body = "
-        Name: $fullname\n
-        E-Mail: $email\n
-        Nachricht:\n$message
-    ";
-
-    // Header setzen
-    $headers = "From: $email\r\n" .
-               "Reply-To: $email\r\n" .
-               "X-Mailer: PHP/" . phpversion();
-
-    // E-Mail senden
-    if (mail($to, $subject, $body, $headers)) {
-        echo "Nachricht wurde erfolgreich gesendet!";
+        $messageStatus = "Bitte füllen Sie alle Felder aus.";
     } else {
-        echo "Es gab ein Problem beim Senden der Nachricht.";
+        // Empfängeradresse und Betreff
+        $to = "mail@about-elias.de"; // Zieladresse
+        $subject = "Neue Nachricht von $fullname"; // Betreff
+
+        // Nachricht formatieren
+        $body = "
+            Name: $fullname\n
+            E-Mail: $email\n
+            Nachricht:\n$message
+        ";
+
+        // Header setzen
+        $headers = "From: $email\r\n" .
+                   "Reply-To: $email\r\n" .
+                   "X-Mailer: PHP/" . phpversion();
+
+        // E-Mail senden
+        if (mail($to, $subject, $body, $headers)) {
+            $messageStatus = "Nachricht wurde erfolgreich gesendet!";
+        } else {
+            $messageStatus = "Es gab ein Problem beim Senden der Nachricht.";
+        }
     }
-} else {
-    echo "Ungültige Anfrage.";
 }
 ?>
